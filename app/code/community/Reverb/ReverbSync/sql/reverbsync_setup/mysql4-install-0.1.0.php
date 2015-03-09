e79aa834864b2428937ccc3183968388085214e8
skonudula <sudheer.konudula@i95dev.com>
2015-03-09 17:12:39 +0530

    Update mysql4-install-0.1.0.php
    pushed old code

--- app/code/community/Reverb/ReverbSync/sql/reverbsync_setup/mysql4-install-0.1.0.php
+++ app/code/community/Reverb/ReverbSync/sql/reverbsync_setup/mysql4-install-0.1.0.php
@@ -4,8 +4,6 @@ $installer  = $this;
 $installer->startSetup();
 $conn = $installer->getConnection();
 
-
-
 try
 {
     $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
@@ -29,25 +27,48 @@ catch (Exception $excp)
 {
     Mage::log($excp->getMessage());  
 }
+try
+{
+    $setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+    $setup->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'rev_product_id', array(
+        'group' => 'General',
+        'backend' => '',
+        'frontend' => '',
+        'label' => 'Rev Product id',
+        'input' => 'hidden',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+        'visible' => 'false',
+        'required' => false,
+        'user_defined' => false,
+        'apply_to' => '',
+        'visible_on_front' => false,
+        'used_in_product_listing' => false
+    ));
+}
+catch (Exception $excp)
+{
+    Mage::log($excp->getMessage());  
+}
 try{
-$setup->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'rev_sync', array(
-    'group' => 'General',
-     'type' => 'int',
-    'backend' => '',
-    'frontend' => '',
-    'label' => 'Sync to Reverb',
-    'input' => 'select',
-    'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
-    'visible' => true,    
-    'required' => false,
-    'default' => '0',
-    'user_defined' => false,
-    'apply_to' => '',
-    'visible_on_front' => false,
-    'source' => 'eav/entity_attribute_source_boolean',
-    'used_in_product_listing' => false
+    $setup->addAttribute(Mage_Catalog_Model_Product::ENTITY, 'rev_sync', array(
+        'group' => 'General',
+        'type' => 'int',
+        'backend' => '',
+        'frontend' => '',
+        'label' => 'Sync to Reverb',
+        'input' => 'select',
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_WEBSITE,
+        'visible' => true,    
+        'required' => false,
+        'default' => '0',
+        'user_defined' => false,
+        'apply_to' => '',
+        'visible_on_front' => false,
+        'source' => 'eav/entity_attribute_source_boolean',
+        'used_in_product_listing' => false
    
-));
+    ));
 }
 catch (Exception $excp)
 {
