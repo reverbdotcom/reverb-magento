5b099c7abd7c375b35254c1ccacca649c560a162
Sean Dunagan <dunagan5887@gmail.com>
2015-08-19 18:44:08 -0500

    Enabling global listing creation kill-switch as well as fixing issues regarding individual product exclusion

---
+++ app/code/community/Reverb/ReverbSync/Model/Exception/Product/Excluded.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author: Sean Dunagan
+ * Created: 8/19/15
+ */
+
+class Reverb_ReverbSync_Model_Exception_Product_Excluded extends Exception
+{
+
+} 
---
+++ app/code/community/Reverb/ReverbSync/Model/Exception/Status/Error.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Author: Sean Dunagan
+ * Created: 8/19/15
+ */
+
+class Reverb_ReverbSync_Model_Exception_Status_Error extends Exception
+{
+
+}
