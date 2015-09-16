f3e462d3d6e3ff9657fdb6323c9c3aeee269bcac
Sean Dunagan <dunagan5887@gmail.com>
2015-09-16 00:29:21 -0500

    Implementing check to ensure reverb order has not already beenn synced before attempting to queue order creation for it

--- app/code/community/Reverb/ReverbSync/Helper/Orders/Retrieval/Creation.php
+++ app/code/community/Reverb/ReverbSync/Helper/Orders/Retrieval/Creation.php
@@ -9,9 +9,29 @@ class Reverb_ReverbSync_Helper_Orders_Retrieval_Creation
 {
     const ORDERS_CREATION_RETRIEVAL_URL_TEMPLATE = '/api/my/orders/selling/all?created_start_date=%s&created_end_date=%s';
     const MINUTES_IN_PAST_FOR_CREATION_QUERY = 1440;
+    const EXCEPTION_CHECK_IF_ORDER_ALREADY_SYNCED = 'An exception occurred while checking to see if order with reverb id %s had already been created in Magento: %s';
 
     public function queueOrderActionByReverbOrderDataObject(stdClass $orderDataObject)
     {
+        $reverb_order_number = $orderDataObject->order_number;
+
+        try
+        {
+            $magento_entity_id = Mage::getResourceSingleton('reverbSync/order')
+                                    ->getMagentoOrderEntityIdByReverbOrderNumber($reverb_order_number);
+            if (!empty($magento_entity_id))
+            {
+                // Order was already synced, don't attempt to queue an order creation
+                // Return true because the calling block will throw an exception if we return an empty() value
+                return true;
+            }
+        }
+        catch(Exception $e)
+        {
+            $error_message = $this->__(self::EXCEPTION_CHECK_IF_ORDER_ALREADY_SYNCED, $reverb_order_number, $e->getMessage());
+            throw new Exception($error_message);
+        }
+
         return $this->_getOrderTaskResourceSingleton()->queueOrderCreationByReverbOrderDataObject($orderDataObject);
     }
 
