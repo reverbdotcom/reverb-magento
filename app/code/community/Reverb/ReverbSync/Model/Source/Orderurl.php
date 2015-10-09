02e9ae887151897411b7761998a6c36415010e8d
Yan Pritzker <ypritzker@Yans-MacBook-Pro.local>
2015-10-09 12:47:32 -0500

    Use updated_start_date for awaiting shipment orders and created_start_date for all orders

--- app/code/community/Reverb/ReverbSync/Model/Source/Orderurl.php
+++ app/code/community/Reverb/ReverbSync/Model/Source/Orderurl.php
@@ -6,8 +6,14 @@
 
 class Reverb_ReverbSync_Model_Source_Orderurl
 {
-    const ALL_ORDERS_URL = '/api/my/orders/selling/all?updated_start_date=%s';
+    const ALL_ORDERS_URL = '/api/my/orders/selling/all?created_start_date=%s';
     const ALL_ORDERS_LABEL = 'All (including Unpaid Accepted Offers)';
+
+    // Not for awaiting shipment, we are using the updated timestamp because
+    // Orders may be created at time X but be awaiting shipment at X+Y.
+    //
+    // Thus it is safer to use the updated timestamp to make sure we don't miss any that were created
+    // before our last sync but changed to awaiting shipment after the last sync.
     const ORDERS_AWAITING_SHIPMENT_URL = '/api/my/orders/selling/awaiting_shipment?updated_start_date=%s';
     const ORDERS_AWAITING_SHIPMENT_LABEL = 'Paid Orders Awaiting Shipment';
 
