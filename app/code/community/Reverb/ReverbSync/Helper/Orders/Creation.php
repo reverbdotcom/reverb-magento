78c6080cc4e897cfac95372e3051c50abe57c85a
Yan Pritzker <ypritzker@Yans-MacBook-Pro.local>
2015-10-29 11:47:53 -0500

    Revert "Merge pull request #133 from reverbdotcom/issues/github_132"
    This reverts commit 335ebf6c29201af0e9fafaf40e5bb1b43cf0e714, reversing
    changes made to 81b6fcaec3c6a24e4e8ef857fcbaffeb0ab09647.

--- app/code/community/Reverb/ReverbSync/Helper/Orders/Creation.php
+++ app/code/community/Reverb/ReverbSync/Helper/Orders/Creation.php
@@ -11,9 +11,6 @@ class Reverb_ReverbSync_Helper_Orders_Creation extends Reverb_ReverbSync_Helper_
     const ERROR_INVALID_SKU = 'An attempt was made to create an order in magento for a Reverb order which had an invalid sku %s';
     const INVALID_CURRENCY_CODE = 'An invalid currency code %s was defined.';
     const EXCEPTION_UPDATE_STORE_NAME = 'An error occurred while setting the store name to %s for order with Reverb Order Id #%s: %s';
-    const EXCEPTION_CONFIGURED_STORE_ID = 'An exception occurred while attempting to load the store with the configured store id of %s: %s';
-
-    const STORE_TO_SYNC_ORDERS_TO_CONFIG_PATH = 'ReverbSync/orders_sync/store_to_sync_order_to';
 
     const REVERB_ORDER_STORE_NAME = 'Reverb';
 
@@ -186,13 +183,7 @@ class Reverb_ReverbSync_Helper_Orders_Creation extends Reverb_ReverbSync_Helper_
 
     protected function _getStoreId()
     {
-        // Check to see if the system configured store id is valid
-        $system_configured_store_id = $this->_getSystemConfigurationStoreId();
-        if ((!is_null($system_configured_store_id)) && ($system_configured_store_id !== false))
-        {
-            // If so return it
-            return $system_configured_store_id;
-        }
+        // TODO: Make the Store Id a configurable setting
 
         // Return the first "real" store Id, falling back to the special Admin store if no stores are defined (unlikely)
         $websites = Mage::app()->getWebsites(true);
@@ -209,24 +200,5 @@ class Reverb_ReverbSync_Helper_Orders_Creation extends Reverb_ReverbSync_Helper_
 
         $website = !is_null($defaultSite) ? $defaultSite : $adminSite;
         return $website->getDefaultGroup()->getDefaultStoreId();
-    }
-
-    protected function _getSystemConfigurationStoreId()
-    {
-        try
-        {
-            $configured_store_id = Mage::getStoreConfig(self::STORE_TO_SYNC_ORDERS_TO_CONFIG_PATH);
-            if (Mage::getSingleton('reverbSync/source_store')->isAValidStoreId($configured_store_id))
-            {
-                return $configured_store_id;
-            }
-        }
-        catch(Exception $e)
-        {
-            $error_message = $this->__(self::EXCEPTION_CONFIGURED_STORE_ID, $configured_store_id, $e->getMessage());
-            Mage::getSingleton('reverbSync/log')->logOrderSyncError($error_message);
-        }
-
-        return false;
     }
 }
