34165f26ae3228873e245e83ba16b30599416c73
Sean Dunagan <dunagan5887@gmail.com>
2015-11-12 23:24:14 -0600

    Allowing Local Pickup orders to be created in Magento

--- app/code/community/Reverb/ReverbSync/Helper/Orders/Creation/Address.php
+++ app/code/community/Reverb/ReverbSync/Helper/Orders/Creation/Address.php
@@ -9,9 +9,16 @@ class Reverb_ReverbSync_Helper_Orders_Creation_Address extends Reverb_ReverbSync
     const ERROR_NO_ADDRESS = 'An attempt was made to create an order in magento for a Reverb order which did not have an address listed';
     const ERROR_VALIDATING_QUOTE_ADDRESS = "While validating a quote address for a Reverb Order Sync, the address failed validation. The address's serialized data was: %s. The error message was: %s";
 
+    const LOCAL_PICKUP_VALUE = 'Local Pickup';
+
     public function addOrderAddressAsShippingAndBillingToQuote(stdClass $reverbOrderObject,
                                                                    Mage_Sales_Model_Quote $quoteToBuild)
     {
+        if ($reverbOrderObject->local_pickup)
+        {
+            $this->_addLocalPickupAddressToOrderObject($reverbOrderObject);
+        }
+
         $shippingAddressObject = $reverbOrderObject->shipping_address;
         if (!is_object($shippingAddressObject))
         {
@@ -23,6 +30,21 @@ class Reverb_ReverbSync_Helper_Orders_Creation_Address extends Reverb_ReverbSync
 
         $this->_addBillingAddressToQuote($customerAddress, $quoteToBuild);
         $this->_getShippingHelper()->addShippingAddressToQuote($reverbOrderObject, $customerAddress, $quoteToBuild);
+    }
+
+    protected function _addLocalPickupAddressToOrderObject(stdClass $reverbOrderObject)
+    {
+        $shippingAddress = new stdClass();
+        $shippingAddress->name = $reverbOrderObject->buyer_name;
+        $shippingAddress->street_address = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->extended_address = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->locality = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->region = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->postal_code = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->country_code = self::LOCAL_PICKUP_VALUE;
+        $shippingAddress->phone = self::LOCAL_PICKUP_VALUE;
+
+        $reverbOrderObject->shipping_address = $shippingAddress;
     }
 
     protected function _addBillingAddressToQuote($customerAddress, $quoteToBuild)
