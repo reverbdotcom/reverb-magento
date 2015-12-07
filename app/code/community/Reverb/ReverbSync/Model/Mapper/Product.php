0a8bd71630d3175dff6bb7251a2384b8fa55443a
Yan Pritzker <ypritzker@Yans-MacBook-Pro.local>
2015-12-07 17:25:05 -0600

    Sync categories on update

--- app/code/community/Reverb/ReverbSync/Model/Mapper/Product.php
+++ app/code/community/Reverb/ReverbSync/Model/Mapper/Product.php
@@ -47,12 +47,7 @@ class Reverb_ReverbSync_Model_Mapper_Product
         }
 
 
-        // We are not syncing categories on update because the category sync is buggy
-        // and is sending over top level categories in the "categories" field, which should
-        // be subcategories. Until that is fixed, we have taken out this call:
-        //
-        // $this->addCategoryToFieldsArray($fieldsArray, $product);
-
+        $this->addCategoryToFieldsArray($fieldsArray, $product);
         $this->addProductConditionIfSet($fieldsArray, $product);
 
         $reverbListingWrapper->setApiCallContentData($fieldsArray);
