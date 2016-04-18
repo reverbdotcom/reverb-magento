350acfedce305c8fd3f5659cd570cde05c989f61
Sean Dunagan <dunagan5887@gmail.com>
2016-04-18 11:03:09 -0500

    #230 Creating database table and ORM structure for Magento attribute to Reverb field mapping

---
+++ app/code/community/Reverb/ReverbSync/Model/Field/Mapping.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Author: Sean Dunagan (https://github.com/dunagan5887)
+ * Created: 4/18/16
+ */
+
+class Reverb_ReverbSync_Model_Field_Mapping extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('reverbSync/field_mapping');
+    }
+}
---
+++ app/code/community/Reverb/ReverbSync/sql/reverbsync_setup/mysql4-upgrade-0.1.15-0.1.16.php
@@ -0,0 +1,49 @@
+<?php
+
+$installer = $this;
+/* @var $installer Mage_Core_Model_Resource_Setup */
+$installer->startSetup();
+
+$magento_reverb_field_mapping_table_name = $this->getTable('reverbSync/magento_reverb_field_mapping');
+
+$installer->getConnection()->dropTable($installer->getTable('reverbSync/magento_reverb_field_mapping'));
+
+$magentoReverbFieldMappingTable =
+    $installer->getConnection()
+        ->newTable($magento_reverb_field_mapping_table_name)
+        ->addColumn(
+            'mapping_id',
+            Varien_Db_Ddl_Table::TYPE_INTEGER,
+            11,
+            array('nullable' => false, 'unsigned' => true, 'primary' => true, 'identity' => true),
+            'Primary Key for the Table'
+        )->addColumn(
+            'magento_attribute_code',
+            Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            255,
+            array('nullable' => false),
+            'Attribute code for the Magento attribute'
+        )->addColumn(
+            'reverb_api_field',
+            Varien_Db_Ddl_Table::TYPE_VARCHAR,
+            255,
+            array('nullable' => false),
+            'Reverb Listing API field'
+        )->addIndex(
+            $installer->getIdxName('reverbSync/magento_reverb_field_mapping', array('magento_attribute_code')),
+            array('magento_attribute_code'),
+            array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX)
+        )->addIndex(
+            $installer->getIdxName('reverbSync/magento_reverb_field_mapping', array('reverb_api_field')),
+            array('reverb_api_field'),
+            array('type' => Varien_Db_Adapter_Interface::INDEX_TYPE_INDEX)
+        )
+        /*
+         * Magento does not have an index in the eav_attribute table where attribute_code is the first column. As such,
+         *  we can't create a foreign key on the attribute_code field
+         */
+        ->setComment('Table abstracting a mapping from Magento attributes to Reverb fields');
+
+$installer->getConnection()->createTable($magentoReverbFieldMappingTable);
+
+$installer->endSetup();
